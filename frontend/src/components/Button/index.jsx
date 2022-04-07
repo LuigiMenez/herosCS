ca7f4642c8b8b0d3bb7b572122390c34863acb0a
LuigiMenez <ltc.menez@gmail.com>
2022-04-07 09:26:05 +0200

    Created a button component

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import Sbutton from "@components/Button/style";
+
+export default function Button({ title, func }) {
+  return (
+    <Sbutton type="button" className="logIn" onClick={func}>
+      {title}
+    </Sbutton>
+  );
+}
+Button.propTypes = {
+  title: PropTypes.string,
+  func: PropTypes.func,
+};
+Button.defaultProps = {
+  title: "",
+  func: () => {},
+};
