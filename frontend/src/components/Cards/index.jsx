c6c981a1a55b9ed1db0fac4325497aa14d40a740
LuigiMenez <ltc.menez@gmail.com>
2022-04-11 14:20:19 +0200

    Created components cards and style

---
+++ frontend/src/components/Cards/index.jsx
@@ -0,0 +1,9 @@
+import SCard from "./style";
+
+export default function Cards() {
+  return (
+    <SCard>
+      <p>Starfinder</p>
+    </SCard>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Cards from "@components/Cards";
 import Header from "@components/Header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Cards />
+    </>
+  );
 }
