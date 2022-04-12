4f73d1fbc582fcfb05ac76af710d3f9f951df9e7
LuigiMenez <ltc.menez@gmail.com>
2022-04-12 15:18:13 +0200

    map of cards components, and props data

--- frontend/src/components/Cards/index.jsx
+++ frontend/src/components/Cards/index.jsx
@@ -1,9 +1,17 @@
 import SCard from "./style";
 
-export default function Cards() {
+export default function Cards({ name, image }) {
   return (
-    <SCard>
-      <p>Starfinder</p>
+    <SCard img={image}>
+      <p>{name}</p>
     </SCard>
   );
 }
+Cards.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+};
+Cards.defaultProps = {
+  image: "",
+  name: () => {},
+};
