3a9b19703edd720b4e0a154beea708dab5a1572f
LuigiMenez <ltc.menez@gmail.com>
2022-04-12 16:51:08 +0200

    Created router and first route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
