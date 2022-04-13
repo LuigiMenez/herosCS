2d9fbfcc190d53c43f0d1e72578f951224b3dfb6
LuigiMenez <ltc.menez@gmail.com>
2022-04-13 12:10:30 +0200

    remove death code with the store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -8,10 +8,6 @@ const rootReducer = combineReducers({ gameReducer });
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
-
-  // /* eslint-disable no-underscore-dangle */
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // /* eslint-disable no-underscore-dangle */
 );
 
 export default store;
