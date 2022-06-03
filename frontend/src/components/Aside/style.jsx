9c95f9ccb2156d74719338337f84296f714f1988
LuigiMenez <ltc.menez@gmail.com>
2022-06-03 11:37:34 +0200

    Style of Aside component

--- frontend/src/components/Aside/style.jsx
+++ frontend/src/components/Aside/style.jsx
@@ -7,12 +7,29 @@ const SAside = styled.section`
   bottom: 0;
   right: 0;
   width: 30vw;
+  height: 100vh;
   .nameTitle {
     width: 100%;
   }
+
+  p,
   img {
+    background: #ffffff7c;
+    border-radius: 15px;
     width: 20vw;
-    margin: 3em;
+  }
+  img {
+    margin: 3em 5vw 0;
+  }
+
+  p {
+    position: absolute;
+    bottom: 1em;
+    right: 1vw;
+    left: 1vw;
+    width: 90%;
+    padding: 0.5em;
+    font-weight: bold;
   }
 `;
 
