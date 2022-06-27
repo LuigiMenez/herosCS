849aaf10471889db1dc19939caba482e14d8f69a
LuigiMenez <ltc.menez@gmail.com>
2022-06-27 16:18:20 +0200

    Create et add components Aside Chara

---
+++ frontend/src/components/AsideChara/index.jsx
@@ -0,0 +1,39 @@
+import tsuna from "@assets/tsunae.jpg";
+import SAsideChara from "./style";
+
+export default function AsideChara() {
+  return (
+    <SAsideChara>
+      <h2>Name</h2>
+      <div className="type">
+        <p>Classe</p>
+        <p>Race</p>
+        <p>lvl: 2</p>
+      </div>
+      <img src={tsuna} alt="" />
+      <div className="container">
+        <div className="columnLeft">
+          <p>Taille: 1.60m</p>
+          <p>Poid: 55 kg</p>
+          <p>Vitesse: 9 m</p>
+          <p>Yeux: Bleu</p>
+        </div>
+        <div className="columnRight">
+          <p>Alignement: CN</p>
+          <p>Divinité: Dieu</p>
+          <p>Sexe: F or M</p>
+          <p>Cheveux: Noir</p>
+        </div>
+      </div>
+      <section>
+        <h3>Biographie :</h3>
+        <p className="bio">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+          deserunt perspiciatis laboriosam quisquam quia necessitatibus totam
+          tempora nisi magnam debitis? Earum placeat tempora voluptatem,
+          voluptatibus explicabo corporis sunt at velit.
+        </p>
+      </section>
+    </SAsideChara>
+  );
+}
---
+++ frontend/src/components/AsideChara/style.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { media } from "../../mediaQueries";
+
+const SAsideChara = styled.section`
+  background: #201853;
+  border-left: 5px solid #2cabd8;
+  display: none;
+  position: absolute;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 30vw;
+  height: 100vh;
+  color: white;
+  ${media("pad")`
+    display: block;
+    `} h2 {
+    font-size: 2em;
+    padding: 0.5em;
+    text-align: center;
+  }
+
+  h2,
+  h3,
+  .type {
+    color: #e27029;
+    font-weight: bold;
+  }
+
+  h3 {
+    font-size: 1.5em;
+    padding-left: 0.5em;
+  }
+
+  .type {
+    display: flex;
+    justify-content: space-around;
+    font-size: 1.2em;
+    p {
+      padding: 0;
+    }
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-around;
+    font-size: 1.2em;
+    width: 90%;
+    margin: auto;
+  }
+
+  img {
+    width: 65%;
+    margin: 1em auto 1em;
+    border: 5px solid #2cabd8;
+  }
+
+  p {
+    padding-top: 0.5em;
+  }
+
+  .bio {
+    width: 90%;
+    margin: auto;
+  }
+  section {
+    position: absolute;
+    bottom: 1em;
+  }
+`;
+
+export default SAsideChara;
